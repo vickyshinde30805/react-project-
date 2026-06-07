fd93c151b9fa2877f10e540322560010afdcdf28
Vicky Shinde <vickyshinde30805@gmail.com>
2026-06-08 01:28:43 +0530

    01 folder done

---
+++ 01 folder/src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 id="parent">App</h1>
+      <h2 id="child">Hellow</h2>
+      <h3 id="grandchild">World</h3>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ script.js
@@ -0,0 +1,3 @@
+let h1=document.createElement("h1");
+h1.innerHTML="Hellow world its JS";
+document.body.appendChild(h1);
